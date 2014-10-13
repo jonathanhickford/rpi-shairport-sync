38c20dda1267aa890a5bc5b0763bf2c3cbf5f9a9
James <james@resin.io>
2014-10-13 16:13:25 +0100

    Update a simple version for Ruby code

--- helloworld.rb
+++ helloworld.rb
@@ -1,12 +1,1 @@
-class SaySomething
-	def initialize(name)
-		@name = name.capitalize
-	end
-
-	def sayHello
-		puts "Hello #{@name}";
-	end
-end
-
-hello = SaySomething.new("Resin")
-hello.sayHello
+puts "Hello, World!"
